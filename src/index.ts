aa285c2cbbb0c62edeade17b9e3c5db8f7e519ff
leemhoon00 <leemhoon000@gmail.com>
2025-01-04 15:59:07 +0900

    feat: send log to slack

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { CloudWatchLogsHandler, CloudWatchLogsEvent } from 'aws-lambda';
 import * as zlib from 'zlib';
-// import { IncomingWebhook } from '@slack/webhook';
+import { IncomingWebhook } from '@slack/webhook';
 
-// const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
 export const handler: CloudWatchLogsHandler = async (
   event: CloudWatchLogsEvent
@@ -11,9 +11,35 @@ export const handler: CloudWatchLogsHandler = async (
 
   const decompressed = zlib.gunzipSync(payload);
 
-  const logData = JSON.parse(decompressed.toString('utf-8'));
+  const logData = JSON.parse(decompressed.toString('utf-8')) as LogData;
 
-  console.log('Decompressed:', logData);
+  const log = logData.logEvents.map((log) => log.message).join('\n');
+
+  if (!SLACK_WEBHOOK_URL) {
+    console.error('SLACK_WEBHOOK_URL is not defined');
+    return;
+  }
+
+  const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+
+  await webhook.send({
+    text: log,
+    username: 'CloudWatchLogs',
+    icon_emoji: ':cloudwatch:',
+  });
 
   return;
+};
+
+type LogData = {
+  messageType: string;
+  owner: string;
+  logGroup: string;
+  logStream: string;
+  subscriptionFilters: string[];
+  logEvents: {
+    id: string;
+    timestamp: number;
+    message: string;
+  }[];
 };
