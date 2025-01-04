b7ebc4fbfbecc3025d1165d8cf49885abde6177a
leemhoon00 <leemhoon000@gmail.com>
2025-01-04 15:34:43 +0900

    base64 디코딩

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import { CloudWatchLogsHandler } from 'aws-lambda';
+import { CloudWatchLogsHandler, CloudWatchLogsEvent } from 'aws-lambda';
 // import { IncomingWebhook } from '@slack/webhook';
 
 // const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
-export const handler: CloudWatchLogsHandler = async (event) => {
-  console.log('Received event:', JSON.stringify(event, null, 2));
+export const handler: CloudWatchLogsHandler = async (
+  event: CloudWatchLogsEvent
+) => {
+  const payload = Buffer.from(event.awslogs.data, 'base64').toString('utf8');
+
+  console.log('Received event:', JSON.stringify(payload, null, 2));
 
   return;
 };
